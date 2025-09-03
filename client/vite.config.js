6a8a8dbec07f71ea01fb4850b96628875e35b332
Mary Cathline <122857396+marycathline@users.noreply.github.com>
2025-09-04 00:16:57 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,10 +12,6 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
+    open: true
+  }
 })
