249d12bdded0addf62c05eeea5ee463234f20026
Mary Cathline <122857396+marycathline@users.noreply.github.com>
2025-09-03 23:36:07 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,8 @@ import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import './styles/global.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-
-      <App />
-
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
